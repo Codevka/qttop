6a857566ee6af285a796b6e17bfbd3f50e5baa18
Codevka <1724707236@qq.com>
2023-05-18 14:07:56 +0800

    feat: basic ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTabBar>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tabWidget->tabBar()->setDocumentMode(true);
+    ui->tabWidget->tabBar()->setExpanding(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
